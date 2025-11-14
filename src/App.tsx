a219d4bbcfd51d903223e29df2277457f7370d3e
Giancarlo Vera <gianverab@gmail.com>
2025-11-14 02:20:27 -0500

    fix: update App with UserProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
+import { UserProvider } from './context/AppContext';
 
 const App = () => {
   return (
-    
+    <UserProvider>
       <SafeAreaView style={styles.container}>
         <StatusBar barStyle="dark-content" backgroundColor="#f5f5f5" />
         <AppNavigator />
       </SafeAreaView>
+    </UserProvider>
   );
 };
 
